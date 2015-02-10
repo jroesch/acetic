8dc6e756128a6d5087579ec4f9fe42484bd59062
Jared Roesch <roeschinc@gmail.com>
2015-02-09 23:19:46 -0800

    Sketch basic core fragment
    Sketch an initial core language that removes incidental complexity from the Rust AST
    the goal is to desugar to a small core language that will be amenable to a small type
    checker useful for experimentation.
    
    The end goal will be to desugar, collect definitions, then type check in as few passes
    as possible. I believe by taking inspiration from bi-directional type checking it will
    be possible to dramatically simplify/collapse the compiler passes.

--- src/core.rs
+++ src/core.rs
@@ -1,6 +1,82 @@
+// A simplified program structure amenable to simple type checking
+// and type environment construction.
+use syntax::ast;
+
+#[derive(Debug)]
+pub struct Name {
+    string: String
+}
+
+impl Name {
+    pub fn from_ident(ident: ast::Ident) -> Name {
+        Name {
+            string: ident.name.as_str().to_string()
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum Item {
-    Fn,
+    Fn(Generics, Vec<Predicate>, Block),
+    Trait(Generics, Vec<Predicate>, Vec<Item>),
+    // Simplify the representation of data types
+    Data(Name, Generics, Vec<Predicate>, Vec<Constructor>)
+}
+
+impl Item {
+    pub fn record_struct(name: Name,
+                         generics: Generics,
+                         predicates: Vec<Predicate>,
+                         fields: Vec<Field>) -> Item {
+
+        Item::Data(name, generics, predicates, vec![Constructor::Record])
+    }
+
+    //fn record_struct(name: Name, generics: Generics, predicates: Predicates) -> Item {
+        //panic!("NYI")j
+    //}
+}
+
+// Represents a sequence of statements.
+#[derive(Debug)]
+enum Block { MkBlock }
+
+#[derive(Debug)]
+enum Field { DummyField }
+
+// Generics are *only* types and lifetimes, bounds are completely seperate.
+#[derive(Debug)]
+pub struct Generics {
+    types: Vec<Ty>,
+    lifetimes: Vec<Lifetime>
+}
+
+impl Generics {
+    pub fn empty() -> Generics {
+        Generics {
+            types: Vec::new(),
+            lifetimes: Vec::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Ty;
+
+#[derive(Debug)]
+pub struct Lifetime;
+
+// A unified concept of a constructor
+#[derive(Debug)]
+enum Constructor {
+    Tuple, // Anonymous fields like struct Foo(...) or an enum constructor
+    Record, // A struct style record constructor
+}
+
+// No associated type projections yet
+#[derive(Debug)]
+enum Predicate {
     Trait,
-    Struct
+    Outlives,
+    Equate
 }
--- src/desugar.rs
+++ src/desugar.rs
@@ -11,9 +11,30 @@ pub struct Desugar {
 impl<'v> Desugar {
     fn desugar_item(&mut self, i: &'v ast::Item) {
         match i.node {
-            ast::Item_::ItemFn(..) => self.items.push(core::Item::Fn),
-            ast::Item_::ItemTrait(..) => self.items.push(core::Item::Trait),
-            ast::Item_::ItemStruct(..) => self.items.push(core::Item::Struct),
+            // ast::Item_::ItemFn(..) => self.items.push(core::Item::Fn),
+            // ast::Item_::ItemTrait(..) => self.items.push(core::Item::Trait),
+            ast::Item_::ItemStruct(ref struct_def, ref ast_generics) => {
+                // Figure out what kind of struct we have.
+                let core_struct = match struct_def.ctor_id {
+                    // Tuple like struct
+                    Some(_)  => { panic!("{:?}", struct_def) }
+                    // Record like struct
+                    None => {
+                        let name = core::Name::from_ident(i.ident);
+                        let generics = core::Generics::empty();
+                        let predicates = Vec::new();
+                        let fields = Vec::new();
+
+                        core::Item::record_struct(
+                            name,
+                            generics,
+                            predicates,
+                            fields)
+                    }
+                };
+
+                self.items.push(core_struct);
+            }
             _ => panic!("Item type not supported!")
         }
     }
